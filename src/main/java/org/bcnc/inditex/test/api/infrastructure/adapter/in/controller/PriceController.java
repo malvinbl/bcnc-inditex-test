8dab4146f8299dc60657689c039c1d6efc9123a8
Malvin <malvinbl@gmail.com>
2024-11-24 15:38:57 +0100

    refactorizando controlador

--- src/main/java/org/bcnc/inditex/test/api/infrastructure/adapter/in/controller/PriceController.java
+++ src/main/java/org/bcnc/inditex/test/api/infrastructure/adapter/in/controller/PriceController.java
@@ -1,5 +1,6 @@
 package org.bcnc.inditex.test.api.infrastructure.adapter.in.controller;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,10 @@ import org.bcnc.inditex.test.api.domain.service.PriceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @Tag(name = "Price Controller")
 @RequiredArgsConstructor
@@ -19,7 +23,17 @@ public class PriceController {
 
     @Operation(summary = "Get pricing details")
     @GetMapping("/details")
-    public ResponseEntity<String> getPricingDetails() {
+    public ResponseEntity<String> getPricingDetails(
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        @RequestParam(name = "date")
+        LocalDateTime date,
+
+        @RequestParam(name = "productId")
+        Long productId,
+
+        @RequestParam(name = "brandId")
+        Long brandId
+    ) {
         String pricingDetails = priceService.getPricingDetails();
         return ResponseEntity.ok(pricingDetails);
     }
